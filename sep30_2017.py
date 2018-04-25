11f60a22dbe7724e219d8950e281e07a44728404
Dev2050 <yebcha2050@hotmail.com>
2018-04-25 11:56:42 +0300

    Add files via upload

---
+++ sep30_2017.py
@@ -0,0 +1,18 @@
+Python 3.7.0a1 (v3.7.0a1:8f51bb4, Sep 19 2017, 18:50:29) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> herList=["Amlak Newu","Be Wistachin Yemiyadir","Wede Ewunet Hulu Mimeran","Yemikatitlin"]
+>>> for i in herList:
+	print(str(i) + ": The Holy Spirit")
+
+	
+Amlak Newu: The Holy Spirit
+Be Wistachin Yemiyadir: The Holy Spirit
+Wede Ewunet Hulu Mimeran: The Holy Spirit
+Yemikatitlin: The Holy Spirit
+>>> '''print("Hawi Gebeyew Duguma(Mignot Gebeyew Ewunet) on September 30 2017")
+print("she told those lists above about The Holy Spirit of God.")
+print(""which is true.)'''
+'print("Hawi Gebeyew Duguma(Mignot Gebeyew Ewunet) on September 30 2017")\nprint("she told those lists above about The Holy Spirit of God.")\nprint(""which is true.)'
+>>> print("Blessed be The Father, The Son, and The Holy Spirit, the one and only true God, The God of Abraham, The God of Isaac, and The God of Jacob, who is the God of the living.")
+Blessed be The Father, The Son, and The Holy Spirit, the one and only true God, The God of Abraham, The God of Isaac, and The God of Jacob, who is the God of the living.
+>>> 
